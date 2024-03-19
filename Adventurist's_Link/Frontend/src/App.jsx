33b694a947c61245e1f19d3a323905b8c45fe0b9
Iheb-JB <113143045+Iheb-JB@users.noreply.github.com>
2024-03-19 23:52:04 +0100

    Users signup procedure + Frontend Header + Signup UI design

--- Adventurist's_Link/Frontend/src/App.jsx
+++ Adventurist's_Link/Frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import React from 'react'
+import Home from './Pages/Home'
+import SignUp from './Pages/SignUp'
+import SignIn from './Pages/SignIn'
+import Header from './Components/Header'
+import About from './Pages/About'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App() {
+  return (   
+    <BrowserRouter >
+      <Header />
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/signin' element={<SignIn />}/>
+          <Route path='/about' element={<About />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
---
+++ Adventurist's_Link/Frontend/src/Pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+      <div>
+        <h1>Welcome to the SingIn Page!</h1>
+        {/* Add your own content here */}
+      </div>
+    );
+  };
+
+export default SignIn;
